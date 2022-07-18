8dff31902d4aac46810406e95c1cd89f07ffa4bc
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-07-18 01:32:55 +0100

    Completed Registration and Login functionality with API backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Formsy from "./pages/Formsy";
+import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        {/* <Route path="/" element={<Formsy />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,24 @@
 import { Button, Form, Input } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import React from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function Register() {
-  const onFinish = (values) => {
-    console.log("Received values of form: ", values);
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/api/user/register", values);
+      if (response.data.success) {
+        toast.success(response.data.message);
+        toast("Redirecting to login page");
+        navigate("/login");
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong comrade!!!");
+    }
   };
 
   return (
