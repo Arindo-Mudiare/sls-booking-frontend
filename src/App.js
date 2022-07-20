b990980820753e60014103d886fe262485d52dbf
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-07-20 12:25:39 +0100

    implemented protected routes and unauthorized routes functionality

--- src/App.js
+++ src/App.js
@@ -4,15 +4,47 @@ import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import { useSelector } from "react-redux";
+import ProtectedRoute from "./components/protectedRoute";
+import PublicRoutes from "./components/publicRoutes";
 
 function App() {
+  const { loading } = useSelector((state) => state.alerts);
   return (
     <BrowserRouter>
+      {loading && (
+        <div className="spinner-parent">
+          <div className="spinner-border" role="status"></div>
+        </div>
+      )}
+
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/login"
+          element={
+            <PublicRoutes>
+              <Login />
+            </PublicRoutes>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoutes>
+              <Register />
+            </PublicRoutes>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,19 @@
 import { Button, Form, Input } from "antd";
-import { Link, useNavigate } from "react-router-dom";
 import React from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "../redux/alertsSlice";
 
 function Login() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const onFinish = async (values) => {
     try {
+      dispatch(showLoading());
       const response = await axios.post("/api/user/login", values);
+      dispatch(hideLoading());
       if (response.data.success) {
         toast.success(response.data.message);
         toast("Redirecting to home page");
@@ -18,6 +23,7 @@ function Login() {
         toast.error(response.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       toast.error("Something went wrong comrade!!!");
     }
   };
